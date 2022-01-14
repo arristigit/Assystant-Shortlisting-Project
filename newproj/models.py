4e1c90f4c04c27df7405b6e01ea936eab7707807
arristigit <arristmedia@gmail.com>
2022-01-14 18:48:40 +0530

    Improved functionalities

--- newproj/models.py
+++ newproj/models.py
@@ -5,6 +5,9 @@ class Department(models.Model):
     dept_name = models.CharField(max_length=100)
     dept_head = models.CharField(max_length=100)
 
+    def __str__(self):
+        return f"{self.dept_id} {self.dept_name} {self.dept_head}"
+
 class Student(models.Model):
     std_id = models.IntegerField(primary_key=True)
     first_name = models.CharField(max_length=100)
@@ -12,7 +15,7 @@ class Student(models.Model):
     departments = models.ManyToManyField(Department, related_name="dept")
 
     def __str__(self):
-        return f"{self.first_name} {self.last_name}"
+        return f"{self.std_id} {self.first_name} {self.last_name}"
 
     def get_full_name(self):
         return f"{self.first_name} {self.last_name}"
--- newproj/views.py
+++ newproj/views.py
@@ -22,16 +22,102 @@ class StudentView(APIView):
         return Response(serializer.data)
 
     def post(self, request, format=None):
-        serializer = StudentSerializer(data=request.data)
-        if serializer.is_valid():
-            student = serializer.save()
+        try:
+            # If one Student and Many Departments (OSMD) inputs
+            if request.data["relationship_type"] == "OSMD":
+                serializer = StudentSerializer(data=request.data)
+                if serializer.is_valid():
+                    student = serializer.save()
 
-            # Add deparment to the Student instance
-            dept = Department.objects.get(dept_id=request.data['departments'])
-            student.departments.add(dept)
+                    if student: 
+                        dept_ids = request.data['dept_ids'].split('-')
+                        dept_names = request.data['dept_names'].split('-')
+                        dept_heads = request.data['dept_heads'].split('-')
 
-            return Response({"message": "Success! The Student has been created."}, status = status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                        for depts in range(0, len(dept_ids)):
+                            department_details = {
+                                'dept_id' : int(dept_ids[depts]),
+                                'dept_name' : dept_names[depts],
+                                'dept_head' : dept_heads[depts]
+                            }
+                            dept_serializer = DepartmentSerializer(data=department_details)
+                            if dept_serializer.is_valid():
+                                dept_save= dept_serializer.save()
+                                student.departments.add(dept_save)
+
+                    return Response({"message": "Success! The Student with his Deparments has been created."}, status = status.HTTP_201_CREATED)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+            if request.data["relationship_type"] == "ODMS":
+                serializer = DepartmentSerializer(data=request.data)
+                if serializer.is_valid():
+                    dept_save = serializer.save()
+
+                    if dept_save:
+                        std_ids = request.data['std_ids'].split('-')
+                        first_names = request.data['first_names'].split('-')
+                        last_names = request.data['last_names'].split('-')
+
+                        for stud in range(0, len(std_ids)):
+                            student_details = {
+                                'std_id' : int(std_ids[stud]),
+                                'first_name' : first_names[stud],
+                                'last_name' : last_names[stud]
+                            }
+                            stud_serializer = StudentSerializer(data=student_details)
+                            if stud_serializer.is_valid():
+                                stud_save= stud_serializer.save()
+                                stud_save.departments.add(dept_save)
+                
+                    return Response({"message": "Success! The Department with its Students has been created."}, status = status.HTTP_201_CREATED)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+                
+            if request.data["relationship_type"] == "MSMD":
+                deptments_save = []
+
+                dept_ids = request.data['dept_ids'].split('-')
+                dept_names = request.data['dept_names'].split('-')
+                dept_heads = request.data['dept_heads'].split('-')
+
+                for dept in range(0, len(dept_ids)):
+                    department_details = {
+                        'dept_id' : int(dept_ids[dept]),
+                        'dept_name' : dept_names[dept],
+                        'dept_head' : dept_heads[dept]
+                    }
+                    serializer = DepartmentSerializer(data=department_details)
+                    if serializer.is_valid():
+                        dept_save = serializer.save()
+                        deptments_save.append(dept_save)
+
+
+                std_ids = request.data['std_ids'].split('-')
+                first_names = request.data['first_names'].split('-')
+                last_names = request.data['last_names'].split('-')
+
+                for stud in range(0, len(std_ids)):
+                    student_details = {
+                        'std_id' : int(std_ids[stud]),
+                        'first_name' : first_names[stud],
+                        'last_name' : last_names[stud]
+                    }
+                    # print(student_details)
+                    stud_serializer = StudentSerializer(data=student_details)
+                    if stud_serializer.is_valid():
+                        # print("serializer is valid..")
+                        stud_save = stud_serializer.save()
+
+                        for dep in range(0, len(deptments_save)):
+                            # print("iteration in departs to add to the studene: ", dep)
+                            print(deptments_save[dep])
+                            stud_save.departments.add(deptments_save[dep])
+
+                    return Response({"message": "Success! The Students with its Departments has been created."}, status = status.HTTP_201_CREATED)
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+            return Response({"Error": "Please provide correct 'relationship_type'."}, status=status.HTTP_400_BAD_REQUEST)
+
+        except Exception as e:
+            return Response({"Error": f"{e}"}, status=status.HTTP_400_BAD_REQUEST)
 
     def put(self, request, pk=None, format=None):
         obj = Student.objects.get(std_id=pk)
@@ -39,10 +125,27 @@ class StudentView(APIView):
         if serializer.is_valid():
             student = serializer.save()
 
+            dept_ids = request.data['dept_ids'].split('-')
+            dept_names = request.data['dept_names'].split('-')
+            dept_heads = request.data['dept_heads'].split('-')
+
+            for dept in range(0, len(dept_ids)):
+                department_details = {
+                    'dept_id' : int(dept_ids[dept]),
+                    'dept_name' : dept_names[dept],
+                    'dept_head' : dept_heads[dept]
+                }
+                new_obj = Department.objects.get(dept_id=department_details['dept_id'])
+                serializer = DepartmentSerializer(new_obj, data=department_details)
+                if serializer.is_valid():
+                    dept_save = serializer.save()
+                    student.departments.add(dept_save)
+
+
             # Add deparment to the Student instance
-            if request.data['departments']:
-                dept = Department.objects.get(dept_id=request.data['departments'])
-                student.departments.add(dept)
+            # if request.data['departments']:
+            #     dept = Department.objects.get(dept_id=request.data['departments'])
+            #     student.departments.add(dept)
 
             return Response({"message":"Success! The Student has been updated completly."}, status=status.HTTP_202_ACCEPTED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -53,10 +156,17 @@ class StudentView(APIView):
         if serialzer.is_valid():
             student = serialzer.save()
 
+            dept_ids = request.data['dept_ids'].split('-')
+
+            for dep in dept_ids:
+                department = Department.objects.get(dept_id=dep)
+                student.departments.remove(department)
+
             # Add deparment to the Student instance
-            if request.data['departments']:
-                dept = Department.objects.get(dept_id=request.data['departments'])
-                student.departments.add(dept)
+            # if request.data['departments']:
+            #     dept = Department.objects.get(dept_id=request.data['departments'])
+            #     student.departments.add(dept)
+            
 
             return Response({"message":"Success! The Student has been updated paritially."}, status=status.HTTP_202_ACCEPTED)
         return Response(serialzer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -73,11 +183,10 @@ class ListStudentView(View):
         depts = Department.objects.all()
         for d in depts:
             dic = {}
-            stu_count = Student.objects.filter(departments__dept_id=d.dept_id).count()
             dic["dept"] = d.dept_name
-            dic["stu_count"] = stu_count
+            dic["stu_count"] = d.dept.all().count()
             ls.append(dic)
-        context = {"ls": ls, "comma": ","}
+        context = {"ls": ls}
         return render(request, "student/studentCount.html", context=context)
 
 
